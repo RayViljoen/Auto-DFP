66b2848a3ae5e096558aba73522a29949e8f1df6
Ray Viljoen <ray@fubra.com>
2011-09-29 16:44:44 +0100

    Changed adJync.js to one function. Much Faster.

--- DFP/js/adSync.js
+++ DFP/js/adSync.js
@@ -1,74 +1,126 @@
-// Get token
-var dfpSyncToken = jQuery('#dfpSyncToken').text();
 
-var i = 0;
+// Self invoking function handles all async spidering and notifications.
+(function (URLs, authToken) {
 
-// Spider all urls and pass html to callback.
-function dfpAjax( url, id, callback ){	
-	jQuery.ajax({
-	  	url: url,
-	  	dataType: 'html',
-	  	context: document.body,
-	  	cache: false,
-	  	success: function(data){ callback( data, url, id); },
-	  	error: function( request ){ console.error( request.responseText + ' - ' + url); }
-	});
-}
+	"use strict";
+
+// ============ VARS ========================
+
+	var numURLs; // Number of urls passed
+
+// ==========================================
 
 
-// Send AJAX notification to create new pending ad slot
-function dfpSlotNotification( newSlot, url, id ){
-		
-	var query = "?new_dfp_tag=" + id + "&dfp_tag_size=" + newSlot + "&dfp_token=" +  dfpSyncToken;
-	
-	dfpAjax( query, function(){
-		console.log('Notification sent for ' + newSlot + ' on page ' + id);
-	});
-}
+	// Reference number of URLs passed.
+	numURLs = (function () {
+
+		var url, num = 0;
+		for (url in URLs ) { if (URLs.hasOwnProperty(url)) { num += 1; } }
+		return num;
+
+	}());
 
 
-// Check html for adSlots and pass addSlot to dfpSlotNotification
-function dfpAdSlots( html, url, id ){
-	
-	var slots = jQuery(html).find('[dfp]');
-	
-	if( slots.length ){
-		jQuery.each( slots, function(){
-			
-			i += 1;
-			console.log(i);
-			
-			var adSlot = jQuery(this).attr('dfp');
-					
-			//dfpSlotNotification(adSlot, url, id);
+	// Sends async notification to createSlotAsync() PHP function
+	function dfpSlotNotification(id, newSlot) {
+
+		var query;
+
+		// Build query as GET only.
+		// Path is irrelevant as plugin loads anywhere hence the authToken.
+
+		query  = "?new_dfp_tag=" + id;			// Page ID
+		query += "&dfp_tag_size=" + newSlot;	// Ad Unit Size
+		query += "&dfp_token=" +  authToken;	// Auth Token
+
+		// Send notification
+		jQuery.get(query, function () {
+
+			console.log(id + ' : ' + newSlot);
+			// Create proper php response to handle ====== TODO
 		});
 	}
-}
 
 
-// Sync button
-jQuery('#dfpSync a').click(function(){
-		
-	// Loope through pages
-	jQuery.each( dfpPageLinks, function( key, val ){
-		
-		dfpAjax( val, key, dfpAdSlots );
+	// Parse ajax response and pass to notifier.
+	function dfpResponseHandler(id, res) {
+
+		// Find all ad slots from res html
+		var slots = jQuery(res).find('[dfp]');
+
+		// Check html contains ad slots
+		if (slots.length) {
+			jQuery.each(slots, function () {
+
+				// Get value of ad slot
+				var adSlot = jQuery(this).attr('dfp');
+
+				// Pass ad slot value to dfpSlotNotification()
+				dfpSlotNotification(id, adSlot);
+			});
+		}
+	}
+
+
+	// Log Error ====== Create gui error TODO
+	function dfpErrorHandler(id, e) {
+		console.log(e);
+	}
+
+
+	// Attach click handler to sync button.
+	jQuery('#dfpSync a').click(function () {
+
+		// Iterate over urls object
+		jQuery.each(URLs, function (id, permalink) {
+
+			// Do async request for each url. 
+			jQuery.ajax({ url: permalink, dataType: 'html',
+
+				// Success Handler.
+				success: function (res) {
+					dfpResponseHandler(id, res);
+				},
+
+				// Error Handler
+				error: function (e) {
+					dfpErrorHandler(id, e);
+				}
+			});
+		});
+		return false; // Null link
 	});
-	return false;
-});
 
 
-// Remove slot
-jQuery('.inventSlots li a').click(function(){
-	
-	var adUnitItem = jQuery(this).parent('li');
-	var adUnit = adUnitItem.text();
-	var query = "?dfp_remove_slot=" + adUnit + "&dfp_token=" + dfpSyncToken;
-	
-	dfpAjax( query, function(){
-		console.log('Slot ' + adUnit + ' removed');
-		adUnitItem.fadeOut(500);
+	// Attach click handler to delete slot.
+	// Sends async notification to delete ad slot - Handled server side
+	jQuery('.inventSlots li a').click(function () {
+
+		var adUnit, query;
+
+		// Get ad unit name
+		adUnit = jQuery(this).parent('li');
+
+		// Build query as GET only.
+		// Path is irrelevant as plugin loads anywhere hence the authToken.
+		query  = "?dfp_remove_slot=" + adUnit.text();
+		query += "&dfp_token=" + authToken;
+
+		// Send notification
+		jQuery.get(query, function () {
+
+			adUnit.fadeOut(500);
+		// Create proper php response to handle ====== TODO
+
+		});
+
+		return false; // Null link
 	});
-	
-	return false;
-});
+
+
+}(siteURLs, dfpAuthToken)); // Pass PHP generated variables.
+
+
+
+// ======================================================================================================
+
